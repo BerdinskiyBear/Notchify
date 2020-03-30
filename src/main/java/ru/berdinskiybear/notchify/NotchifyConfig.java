4cf744ea64283076584669681aae8135935d858b
BerdinskiyBear <bad.system000@gmail.com>
2020-03-31 00:37:05 +0700

    Moved default values to the constructor

--- src/main/java/ru/berdinskiybear/notchify/NotchifyConfig.java
+++ src/main/java/ru/berdinskiybear/notchify/NotchifyConfig.java
@@ -5,13 +5,20 @@ import com.google.gson.annotations.SerializedName;
 public class NotchifyConfig {
 
     @SerializedName("enchanted_golden_apple_cost")
-    private int egappleEnchantmentCost = 39;
+    private int egappleEnchantmentCost;
     @SerializedName("creative_player_always_succeeds")
-    private boolean creativePlayerAlwaysSucceeds = true;
+    private boolean creativePlayerAlwaysSucceeds;
     @SerializedName("survival_player_always_succeeds")
-    private boolean survivalPlayerAlwaysSucceeds = false;
+    private boolean survivalPlayerAlwaysSucceeds;
     @SerializedName("enchanting_chance_modifier")
-    private float enchantingChanceModifier = 1.0F;
+    private float enchantingChanceModifier;
+
+    public NotchifyConfig() {
+        egappleEnchantmentCost = 39;
+        creativePlayerAlwaysSucceeds = true;
+        survivalPlayerAlwaysSucceeds = false;
+        enchantingChanceModifier = 1.0F;
+    }
 
     public int getEGAppleEnchantmentCost() {
         return egappleEnchantmentCost;
