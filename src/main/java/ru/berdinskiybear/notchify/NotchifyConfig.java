8ee26dadfe52d7ca0386875c77db62c11536e7bd
BerdinskiyBear <bad.system000@gmail.com>
2020-03-31 01:06:19 +0700

    Added most important switches into config

--- src/main/java/ru/berdinskiybear/notchify/NotchifyConfig.java
+++ src/main/java/ru/berdinskiybear/notchify/NotchifyConfig.java
@@ -4,6 +4,10 @@ import com.google.gson.annotations.SerializedName;
 
 public class NotchifyConfig {
 
+    @SerializedName("enable_enchanting_using_anvil")
+    private boolean enableAnvil;
+    @SerializedName("enable_enchanting_using_enchanting_table")
+    private boolean enableEnchantingTable;
     @SerializedName("enchanted_golden_apple_cost")
     private int egappleEnchantmentCost;
     @SerializedName("creative_player_always_succeeds")
@@ -14,10 +18,20 @@ public class NotchifyConfig {
     private float enchantingChanceModifier;
 
     public NotchifyConfig() {
+        enableAnvil = true;
+        enableEnchantingTable = true;
         egappleEnchantmentCost = 39;
         creativePlayerAlwaysSucceeds = true;
         survivalPlayerAlwaysSucceeds = false;
         enchantingChanceModifier = 1.0F;
+    }
+
+    public boolean isAnvilEnabled() {
+        return enableAnvil;
+    }
+
+    public boolean isEnchantingTableEnabled() {
+        return enableEnchantingTable;
     }
 
     public int getEGAppleEnchantmentCost() {
