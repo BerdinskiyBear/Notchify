179d8083bea95e080f64f0d12430e5a597a2eeeb
BerdinskiyBear <bad.system000@gmail.com>
2020-03-30 23:30:56 +0700

    Created a method for loading configs

--- src/main/java/ru/berdinskiybear/notchify/NotchifyMod.java
+++ src/main/java/ru/berdinskiybear/notchify/NotchifyMod.java
@@ -1,9 +1,16 @@
 package ru.berdinskiybear.notchify;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.FabricLoader;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class NotchifyMod implements ModInitializer {
 
@@ -12,9 +19,11 @@ public class NotchifyMod implements ModInitializer {
     public static final String MOD_ID = "notchify";
     public static final String MOD_NAME = "Notchify";
 
+    private static NotchifyConfig currentConfig;
+
     @Override
     public void onInitialize() {
-        //TODO: Initializer, Configs maybe?..
+        loadConfig();
     }
 
     public static void log(String message) {
@@ -25,4 +34,33 @@ public class NotchifyMod implements ModInitializer {
         LOGGER.log(level, "["+MOD_NAME+"] " + message);
     }
 
+    public static void loadConfig() {
+        log("Loading config...");
+        Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+        File configFile = new File(FabricLoader.INSTANCE.getConfigDirectory(), MOD_ID + ".json");
+        if (configFile.exists()) {
+            try {
+                FileReader fileReader = new FileReader(configFile);
+                currentConfig = gson.fromJson(fileReader, NotchifyConfig.class);
+            } catch (IOException exception) {
+                log(Level.ERROR, "Unable to read the config file");
+                log(Level.ERROR, exception.getLocalizedMessage());
+                currentConfig = new NotchifyConfig();
+            }
+        } else {
+            log(Level.WARN, "No config file found, creating new one");
+            currentConfig = new NotchifyConfig();
+            try {
+                FileWriter fileWriter = new FileWriter(configFile);
+                gson.toJson(currentConfig, fileWriter);
+            } catch (IOException exception) {
+                log(Level.ERROR, "Unable to create and/or write config file " + configFile.getAbsolutePath() + configFile.getName());
+                log(Level.ERROR, exception.getLocalizedMessage());
+            }
+        }
+    }
+
+    public static NotchifyConfig getConfig() {
+        return currentConfig;
+    }
 }
