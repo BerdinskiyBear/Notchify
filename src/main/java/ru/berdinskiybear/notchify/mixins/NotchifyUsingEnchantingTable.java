41c6a414e495827c133eff76a0a713517c3c5e79
BerdinskiyBear <bad.system000@gmail.com>
2020-03-31 12:11:35 +0700

    Configs now affect enchanting table

--- src/main/java/ru/berdinskiybear/notchify/mixins/NotchifyUsingEnchantingTable.java
+++ src/main/java/ru/berdinskiybear/notchify/mixins/NotchifyUsingEnchantingTable.java
@@ -43,118 +43,121 @@ public abstract class NotchifyUsingEnchantingTable extends Container {
 
     @Inject(method = "onContentChanged", at = @At(value = "HEAD"), cancellable = true)
     public void notchificationCalculations(Inventory inv, CallbackInfo info) {
-        if (inv == this.inventory) {
-            ItemStack enchantingStack = inv.getInvStack(0);
-            if (enchantingStack.getItem() == Items.GOLDEN_APPLE && enchantingStack.getCount() == 1) {
-                this.context.run((world, blockPos) -> {
-                    int bookshelves = 0;
+        if (NotchifyMod.getConfig().isEnchantingTableEnabled()) {
+            if (inv == this.inventory) {
+                ItemStack enchantingStack = inv.getInvStack(0);
+                if (enchantingStack.getItem() == Items.GOLDEN_APPLE && enchantingStack.getCount() == 1) {
+                    this.context.run((world, blockPos) -> {
+                        int bookshelves = 0;
 
-                    for (int i = -1; i <= 1; ++i) {
-                        for (int j = -1; j <= 1; ++j) {
-                            if (!(i == 0 && j == 0) && world.isAir(blockPos.add(j, 0, i)) && world.isAir(blockPos.add(j, 1, i))) {
-                                if (world.getBlockState(blockPos.add(j * 2, 0, i * 2)).getBlock() == Blocks.BOOKSHELF) {
-                                    ++bookshelves;
-                                }
-
-                                if (world.getBlockState(blockPos.add(j * 2, 1, i * 2)).getBlock() == Blocks.BOOKSHELF) {
-                                    ++bookshelves;
-                                }
-
-                                if (j != 0 && i != 0) {
-                                    if (world.getBlockState(blockPos.add(j * 2, 0, i)).getBlock() == Blocks.BOOKSHELF) {
+                        for (int i = -1; i <= 1; ++i) {
+                            for (int j = -1; j <= 1; ++j) {
+                                if (!(i == 0 && j == 0) && world.isAir(blockPos.add(j, 0, i)) && world.isAir(blockPos.add(j, 1, i))) {
+                                    if (world.getBlockState(blockPos.add(j * 2, 0, i * 2)).getBlock() == Blocks.BOOKSHELF) {
                                         ++bookshelves;
                                     }
 
-                                    if (world.getBlockState(blockPos.add(j * 2, 1, i)).getBlock() == Blocks.BOOKSHELF) {
+                                    if (world.getBlockState(blockPos.add(j * 2, 1, i * 2)).getBlock() == Blocks.BOOKSHELF) {
                                         ++bookshelves;
                                     }
 
-                                    if (world.getBlockState(blockPos.add(j, 0, i * 2)).getBlock() == Blocks.BOOKSHELF) {
-                                        ++bookshelves;
-                                    }
+                                    if (j != 0 && i != 0) {
+                                        if (world.getBlockState(blockPos.add(j * 2, 0, i)).getBlock() == Blocks.BOOKSHELF) {
+                                            ++bookshelves;
+                                        }
 
-                                    if (world.getBlockState(blockPos.add(j, 1, i * 2)).getBlock() == Blocks.BOOKSHELF) {
-                                        ++bookshelves;
+                                        if (world.getBlockState(blockPos.add(j * 2, 1, i)).getBlock() == Blocks.BOOKSHELF) {
+                                            ++bookshelves;
+                                        }
+
+                                        if (world.getBlockState(blockPos.add(j, 0, i * 2)).getBlock() == Blocks.BOOKSHELF) {
+                                            ++bookshelves;
+                                        }
+
+                                        if (world.getBlockState(blockPos.add(j, 1, i * 2)).getBlock() == Blocks.BOOKSHELF) {
+                                            ++bookshelves;
+                                        }
                                     }
                                 }
                             }
                         }
-                    }
 
-                    this.random.setSeed(this.seed.get());
+                        this.random.setSeed(this.seed.get());
 
-                    for (int i = 0; i < 3; i++) {
-                        this.enchantmentPower[i] = NotchificationStuffs.calculateEnchantmentPower(this.random, i, bookshelves);
-                        this.enchantmentId[i] = -1;
-                        this.enchantmentLevel[i] = -1;
-                        if (this.enchantmentPower[i] < i + 1) this.enchantmentPower[i] = 0;
-                    }
-
-                    for (int i = 0; i < 3; i++)
-                        if (this.enchantmentPower[i] > 0) {
-                            this.enchantmentId[i] = Registry.ENCHANTMENT.getRawId(Enchantments.EFFICIENCY);
-                            this.enchantmentLevel[i] = i + 1;
+                        for (int i = 0; i < 3; i++) {
+                            this.enchantmentPower[i] = NotchificationStuffs.calculateEnchantmentPower(this.random, i, bookshelves);
+                            this.enchantmentId[i] = -1;
+                            this.enchantmentLevel[i] = -1;
+                            if (this.enchantmentPower[i] < i + 1) this.enchantmentPower[i] = 0;
                         }
 
-                    this.sendContentUpdates();
-                });
+                        for (int i = 0; i < 3; i++)
+                            if (this.enchantmentPower[i] > 0) {
+                                this.enchantmentId[i] = Registry.ENCHANTMENT.getRawId(Enchantments.EFFICIENCY);
+                                this.enchantmentLevel[i] = i + 1;
+                            }
 
-                info.cancel();
+                        this.sendContentUpdates();
+                    });
+
+                    info.cancel();
+                }
             }
         }
     }
 
     @Inject(method = "onButtonClick", at = @At(value = "HEAD"), cancellable = true)
     public void notchification(PlayerEntity player, int id, CallbackInfoReturnable<Boolean> info) {
-        ItemStack enchantingStack = this.inventory.getInvStack(0);
+        if (NotchifyMod.getConfig().isEnchantingTableEnabled()) {
+            ItemStack enchantingStack = this.inventory.getInvStack(0);
 
-        if (enchantingStack.getItem() == Items.GOLDEN_APPLE && enchantingStack.getCount() == 1) {
-            ItemStack lapisStack = this.inventory.getInvStack(1);
-            int button = id + 1;
+            if (enchantingStack.getItem() == Items.GOLDEN_APPLE && enchantingStack.getCount() == 1) {
+                ItemStack lapisStack = this.inventory.getInvStack(1);
+                int button = id + 1;
 
-            if ((!lapisStack.isEmpty() && lapisStack.getCount() >= button && player.experienceLevel > button && player.experienceLevel > this.enchantmentPower[id]) || player.abilities.creativeMode) {
-                this.context.run((world, blockPos) -> {
-                    this.random.setSeed((long) (this.seed.get() + id + 3));
-                    int playerChance = enchantmentPower[id];
-                    int cap = 39 * 10;
-                    int choice = this.random.nextInt(cap);
+                if ((!lapisStack.isEmpty() && lapisStack.getCount() >= button && player.experienceLevel > button && player.experienceLevel > this.enchantmentPower[id]) || player.abilities.creativeMode) {
+                    this.context.run((world, blockPos) -> {
+                        this.random.setSeed((long) (this.seed.get() + id + 3));
+                        float playerChance = ((float) (enchantmentPower[id]) * NotchifyMod.getConfig().getEnchantingChanceModifier()) / ((float) (NotchifyMod.getConfig().getEGAppleEnchantmentCost()) * 10.0F);
+                        float choice = this.random.nextFloat();
 
-                    NotchifyMod.log(playerChance + "/" + (cap - 1) + ", chosen " + choice);
+                        //NotchifyMod.log(playerChance * 100.0F + "%");
 
-                    player.applyEnchantmentCosts(null, button);
+                        player.applyEnchantmentCosts(null, button);
 
-                    if (!player.abilities.creativeMode) {
-                        lapisStack.decrement(button);
-                        if (lapisStack.isEmpty())
-                            this.inventory.setInvStack(1, ItemStack.EMPTY);
-                    }
+                        if (!player.abilities.creativeMode) {
+                            lapisStack.decrement(button);
+                            if (lapisStack.isEmpty())
+                                this.inventory.setInvStack(1, ItemStack.EMPTY);
+                        }
 
-                    if (choice < playerChance || player.abilities.creativeMode) {
-                        ItemStack newApple = new ItemStack(Items.ENCHANTED_GOLDEN_APPLE, 1);
+                        if ((choice < playerChance || (player.abilities.creativeMode && NotchifyMod.getConfig().isCreativePlayerAlwaysSuccessful())) || NotchifyMod.getConfig().isSurvivalPlayerAlwaysSuccessful()) {
+                            ItemStack newApple = new ItemStack(Items.ENCHANTED_GOLDEN_APPLE, 1);
 
-                        if (enchantingStack.hasCustomName())
-                            newApple.setCustomName(enchantingStack.getName());
+                            if (enchantingStack.hasCustomName())
+                                newApple.setCustomName(enchantingStack.getName());
 
-                        this.inventory.setInvStack(0, newApple);
+                            this.inventory.setInvStack(0, newApple);
 
-                        player.incrementStat(Stats.ENCHANT_ITEM);
+                            player.incrementStat(Stats.ENCHANT_ITEM);
 
-                        if (player instanceof ServerPlayerEntity)
-                            Criterions.ENCHANTED_ITEM.trigger((ServerPlayerEntity) player, enchantingStack, button);
+                            if (player instanceof ServerPlayerEntity)
+                                Criterions.ENCHANTED_ITEM.trigger((ServerPlayerEntity) player, enchantingStack, button);
 
-                        world.playSound(null, blockPos, SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.BLOCKS, 1.0F, world.random.nextFloat() * 0.1F + 0.9F);
-                    } else {
-                        world.playSound(null, blockPos, SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.BLOCKS, 1.0F, world.random.nextFloat() * 0.1F + 2.0F);
-                    }
+                            world.playSound(null, blockPos, SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.BLOCKS, 1.0F, world.random.nextFloat() * 0.1F + 0.9F);
+                        } else {
+                            world.playSound(null, blockPos, SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.BLOCKS, 1.0F, world.random.nextFloat() * 0.1F + 2.0F);
+                        }
 
-                    this.inventory.markDirty();
-                    this.seed.set(player.getEnchantmentTableSeed());
-                    this.onContentChanged(this.inventory);
-                });
+                        this.inventory.markDirty();
+                        this.seed.set(player.getEnchantmentTableSeed());
+                        this.onContentChanged(this.inventory);
+                    });
 
-                info.setReturnValue(true);
-            } else {
-                info.setReturnValue(false);
+                    info.setReturnValue(true);
+                } else {
+                    info.setReturnValue(false);
+                }
             }
         }
     }
