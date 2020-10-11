9b50091904234e3fc9a0bf7ed43ae15acc7a2c31
BerdinskiyBear <56161020+berdinskiybear@users.noreply.github.com>
2020-10-12 00:46:45 +0700

    Enchanted golden apples can now be de-enchanted

---
+++ src/main/java/ru/berdinskiybear/notchify/mixins/UnNotchifyUsingGrindstone.java
@@ -0,0 +1,36 @@
+package ru.berdinskiybear.notchify.mixins;
+
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.GrindstoneScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GrindstoneScreenHandler.class)
+public abstract class UnNotchifyUsingGrindstone extends ScreenHandler {
+    protected UnNotchifyUsingGrindstone(@Nullable ScreenHandlerType<?> type, int syncId) {
+        super(type, syncId);
+    }
+
+    @Shadow @Final private Inventory input;
+    @Shadow @Final private Inventory result;
+
+    @Inject(method = "updateResult", at = @At(value = "HEAD"), cancellable = true)
+    public void unnotchification(CallbackInfo callbackInfo) {
+        ItemStack itemStack1 = this.input.getStack(0);
+        ItemStack itemStack2 = this.input.getStack(1);
+        if ((itemStack1.isEmpty() && itemStack2.getItem() == Items.ENCHANTED_GOLDEN_APPLE) || (itemStack2.isEmpty() && itemStack1.getItem() == Items.ENCHANTED_GOLDEN_APPLE)) {
+            this.result.setStack(0, new ItemStack(Items.GOLDEN_APPLE, itemStack1.isEmpty() ? itemStack2.getCount() : itemStack1.getCount()));
+            this.sendContentUpdates();
+            callbackInfo.cancel();
+        }
+    }
+}
---
+++ src/main/java/ru/berdinskiybear/notchify/mixins/UnNotchifyUsingGrindstoneInsertion.java
@@ -0,0 +1,24 @@
+package ru.berdinskiybear.notchify.mixins;
+
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.slot.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = {"net.minecraft.screen.GrindstoneScreenHandler$2", "net.minecraft.screen.GrindstoneScreenHandler$3"})
+public class UnNotchifyUsingGrindstoneInsertion extends Slot {
+
+    public UnNotchifyUsingGrindstoneInsertion(Inventory inventory, int index, int x, int y) {
+        super(inventory, index, x, y);
+    }
+
+    @Inject(method = "canInsert(Lnet/minecraft/item/ItemStack;)Z", at = @At(value = "HEAD"), cancellable = true)
+    public void insertion(ItemStack stack, CallbackInfoReturnable<Boolean> info) {
+        if (stack.getItem() == Items.ENCHANTED_GOLDEN_APPLE)
+            info.setReturnValue(true);
+    }
+}
