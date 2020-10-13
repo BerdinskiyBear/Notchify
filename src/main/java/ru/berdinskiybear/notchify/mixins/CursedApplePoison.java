e229672d686a638f8d0d765ae8ddb50ab342ddc3
BerdinskiyBear <bad.system000@gmail.com>
2020-10-13 16:31:14 +0700

    Cursed enchanted golden apple will give nausea

---
+++ src/main/java/ru/berdinskiybear/notchify/mixins/CursedApplePoison.java
@@ -0,0 +1,30 @@
+package ru.berdinskiybear.notchify.mixins;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public abstract class CursedApplePoison extends Entity {
+    public CursedApplePoison(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "applyFoodEffects", at = @At(value = "HEAD"))
+    public void applyCursedApplePoison(ItemStack stack, World world, LivingEntity targetEntity, CallbackInfo callbackInfo) {
+        if (!world.isClient() && stack.getItem() == Items.ENCHANTED_GOLDEN_APPLE && EnchantmentHelper.get(stack).containsKey(Enchantments.VANISHING_CURSE)) {
+            targetEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 3000));
+        }
+    }
+}
