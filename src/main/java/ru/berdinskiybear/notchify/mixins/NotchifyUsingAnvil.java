05486e703e37b37052c28c3174d125d118fd4b59
BerdinskiyBear <bad.system000@gmail.com>
2020-11-03 14:06:52 +0700

    Uninvented the wheel: used vanilla function to check items' tags

--- src/main/java/ru/berdinskiybear/notchify/mixins/NotchifyUsingAnvil.java
+++ src/main/java/ru/berdinskiybear/notchify/mixins/NotchifyUsingAnvil.java
@@ -3,7 +3,7 @@ package ru.berdinskiybear.notchify.mixins;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtHelper;
 import net.minecraft.screen.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -15,8 +15,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import ru.berdinskiybear.notchify.NotchifyMod;
-import java.util.Objects;
-import java.util.function.BiFunction;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class NotchifyUsingAnvil extends ForgingScreenHandler {
@@ -38,25 +36,13 @@ public abstract class NotchifyUsingAnvil extends ForgingScreenHandler {
             if (leftStack.getItem() == Items.GOLDEN_APPLE && leftStack.getCount() == 1 && (StringUtils.isBlank(this.newItemName) || (leftStack.hasCustomName() && leftStack.getName().asString().equals(this.newItemName)))) {
                 ItemStack rightStack = this.input.getStack(1);
 
-                BiFunction<CompoundTag, CompoundTag, Boolean> has = (itemTags, requiredTags) -> {
-                    if (requiredTags == null) return true;
-                    else if (requiredTags.isEmpty()) return true;
-                    else if (itemTags == null) return false;
-                    else if (itemTags.isEmpty()) return false;
-                    else {
-                        for (String key: requiredTags.getKeys())
-                            if (!Objects.equals(itemTags.get(key), requiredTags.get(key))) return false;
-                        return true;
-                    }
-                };
-
                 if ( // если
                         ( // либо
                                 NotchifyMod.getConfig().isSecondaryItemRequired() // второй предмет необходим
                                         && rightStack.getItem() == Registry.ITEM.get(new Identifier(NotchifyMod.getConfig().getSecondaryItemID())) // и предмет как в настройках
                                         && rightStack.getCount() == NotchifyMod.getConfig().getSecondaryItemAmount() // и предметов количество как в настройках
                                         && (!NotchifyMod.getConfig().isSecondaryItemNbtEnabled() // и если NBT необходим
-                                                        || has.apply(rightStack.getTag(), NotchifyMod.getConfig().getSecondaryItemNbt())) // и теги предмета соответствуют тегам
+                                                        || NbtHelper.matches(NotchifyMod.getConfig().getSecondaryItemNbt(), rightStack.getTag(), true)) // и теги предмета соответствуют тегам
                         ) || ( // либо
                                 !NotchifyMod.getConfig().isSecondaryItemRequired() // второй предмет не нужен
                                         && rightStack.isEmpty() // и второго предмета нет
